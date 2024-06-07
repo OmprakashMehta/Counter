2c93a0b8837c23a898e1d4208d3615e69bbcaaaf
OmprakashMehta <Omprakashverma090909@gmail.com>
2024-06-07 11:44:31 +0530

    update

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let count = 0;
 
 // Function to increment counter
 function increment() {
-  count++;
+  count = count + 1;
   document.getElementById("counter").innerHTML = count;
 }
 
 // Function to decrement counter
 function decrement() {
   if (count > 0) {
-    count--;
+    count = count - 1;
     document.getElementById("counter").innerHTML = count;
   }
 }
